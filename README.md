c90c2c7e807b81879126e9a863ea71abb8c6e5b2
李尚恒 <lishangheng@han-zi.cn>
2021-01-16 10:37:27 +0800

    增加安装内容

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 <!--
  * @Date: 2021-01-14 14:56:52
  * @LastEditors: LiShangHeng
- * @LastEditTime: 2021-01-14 14:57:44
- * @FilePath: /Study-Notes/Linux/myLinux/README.md
+ * @LastEditTime: 2021-01-16 10:37:27
+ * @FilePath: /myLinux/README.md
 -->
 
 # 简介
 
 因为有很多服务器要登录查看日志或者调试,所以需要有一个适合的自己的使用环境配置.
-注意不要破坏原服务器的配置.# myLinuxConfig
+注意不要破坏原服务器的配置.
+
+# myLinuxConfig安装
+
+```bash
+git clone https://github.com/shanghengv5/myLinuxConfig
+bash myLinuxConfig/lsh_install.sh && . myLinuxConfig/scripts/lsh_env.sh
+```
--- scripts/lsh_env.sh
+++ scripts/lsh_env.sh
@@ -2,15 +2,15 @@
 ###
  # @Date: 2021-01-14 14:58:56
  # @LastEditors: LiShangHeng
- # @LastEditTime: 2021-01-16 10:07:46
- # @FilePath: /Study-Notes/Linux/myLinux/scripts/lsh_env.sh
+ # @LastEditTime: 2021-01-16 10:35:24
+ # @FilePath: /myLinux/scripts/lsh_env.sh
 ### 
 
 export lsh_scripts=/disk2/lsh/scripts;
 export lsh_taillog=$lsh_scripts/lsh_taillog.sh;
 export lsh_pull=$lsh_scripts/lsh_pull.sh;
 
-# export $apiDir="/disk2/www/pcwz/testing/chatapi";
+# export apiDir="/disk2/www/pcwz/testing/chatapi";
 # export socketc="php $apiDir/artisan socket";
 # export worksocket="$socketc:work ";
 # export registersocket="$socketc:register ";
